5f9560494d387c5bd21d276f97d7b0182b659124
ousman-ali <oasgerbalhl@gmail.com>
2024-06-15 13:13:29 -0700

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@
  const Contact = () => {
    return (
      <div className='contact-us'>
-       <h1>This is Contact<span>Page</span></h1>
+       <h1>This is Contact<span> Page</span></h1>
      </div>
    )
  }
